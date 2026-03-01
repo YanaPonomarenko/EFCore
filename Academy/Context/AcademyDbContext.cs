504e22b8f86a154e92f993a6530cf34f056af94a
Yana Ponomarenko <y.ponomar05@gmail.com>
2026-03-01 17:41:48 +0200

    innitial commit

---
+++ Academy/Context/AcademyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Academy.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy.Context;
+
+public class AcademyDbContext:DbContext
+{
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public AcademyDbContext(DbContextOptions options):base(options)
+    {
+        
+    }
+
+    //public DbSet<Teacher> Teachers { get; set; }
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+        //base.OnConfiguring(optionsBuilder);
+        //optionsBuilder.UseSqlServer("Server=QQ12\\SQLEXPRESS;Database=academyPV521;Trusted_Connection=True;TrustServerCertificate=True;");
+    //}
+}
---
+++ Academy/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy.Entities;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Surname{ get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+    public int GroupId { get; set; }
+    public Group Group { get; set; } 
+}
