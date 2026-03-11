e5d27020b47c2ae2f3a3c07400dac628a1a83324
Yana Ponomarenko <y.ponomar05@gmail.com>
2026-03-11 16:21:44 +0200

    added new projects

---
+++ Shop.Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public ICollection<CategoryProduct> CategoryProducts { get; set; } = new List<CategoryProduct>();
+}
---
+++ ShopApp/Data/ShopDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.App.Configurators;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.App.Data;
+
+public class ShopDbContext:DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<CategoryProduct> CategoryProducts { get; set; }
+    public ShopDbContext(DbContextOptions<ShopDbContext> options): base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UseConfiguration ());
+        modelBuilder.ApplyConfiguration(new CategoryProductConfiguration());
+    }
+}
