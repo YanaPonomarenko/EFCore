cf6df6a3cd631c7e641991c630e9dfc12b34d737
Yana Ponomarenko <y.ponomar05@gmail.com>
2026-03-16 17:50:59 +0200

    added h/w 04.03

---
+++ ShopApp/Configurators/OrderItemConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.App.Configurators;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.ToTable("OrderItems");
+
+        builder.HasKey(oi => oi.Id);
+
+        builder.Property(oi => oi.OrderId)
+               .IsRequired();
+
+        builder.Property(oi => oi.ProductId)
+               .IsRequired();
+
+        builder.Property(oi => oi.Quantity)
+               .IsRequired();
+
+        builder.Property(oi => oi.Price)
+               .IsRequired()
+               .HasColumnType("decimal(18,2)");
+
+        builder.HasOne(oi => oi.Order)
+               .WithMany(o => o.OrderItems)
+               .HasForeignKey(oi => oi.OrderId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
